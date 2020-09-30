bcdd001e81b08fcdead0d7ff2992363bf5f8b61b
massartval <massart.val@gmail.com>
2020-09-30 14:38:53 +0200

    3.1.js (update) + 3.2.js

--- 3.2.js
+++ 3.2.js
@@ -1,6 +1,7 @@
+
 let array = [1, 2, 3, 4, 5];
 let total = 0;
 for (let elem of array) {
     total = total + elem;
 }
-console.log('The total is ' + total);
+console.log('The average is ' + (total / array.length));
