472c3024f6065784e1db2ce476dd2c5fbbddf402
massartval <massart.val@gmail.com>
2020-09-30 16:27:54 +0200

    Added documentaion for 4.1.js and 4.2.js

--- 4.1.js
+++ 4.1.js
@@ -3,6 +3,13 @@ const readlineSync = require("readline-sync");
 let a = readlineSync.question('Please enter the width of the rectangle: ');
 let b = readlineSync.question('Please enter the height of the rectangle: ');
 
+/**
+ * Returns a multiplied by b.
+ *
+ * @param {number} a The width of the rectangle.
+ * @param {number} b The height of the rectangle.
+ * @return {number} The surface of the rectangle.
+ */
 function calcSurface(a, b) {
     return a * b;
 }
--- 4.2.js
+++ 4.2.js
@@ -1,3 +1,10 @@
+/**
+ * Returns a random number between 1 and 10.
+ *
+ * Math.random generates a random decimal number between 0 and 1.
+ * Math.floor rounds a number downwards to the nearest integer.
+ * @return {number} The random number between 1 and 10.
+ */
 function rand10() {
     return Math.floor((Math.random() * 10) + 1);
 }
